0d5044c761322894edeeff8825adf4cde603ba22
Owali Ullah Shawon <oshawon@gmail.com>
2022-04-16 15:49:32 +0600

    database connect

--- App.py
+++ App.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = 'secret'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/crud-flask'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Data(db):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+    def __init__(self, name, email, phone):  # constructor
+        self.name = name
+        self.email = email
+        self.phone = phone
 
 
 @app.route("/")
